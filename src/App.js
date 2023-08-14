47079d23b51a2efca17a771dcd7bf20516956cae
saradonnelly97 <sara.donnelly8@gmail.com>
2023-08-14 13:31:02 -0400

    added helper.js, added Suspense

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
+import { createResource as fetchData } from './helper'
 import Gallery from './Components/Gallery'
 import SearchBar from './Components/SearchBar'
 import './App.css';
+import Spinner from './Components/Spinner';
 
 
 const App = () => {
-  let [search, setSearch] = useState('')
+  let [searchTerm, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
-  let [data, setData] = useState([])
+  let [data, setData] = useState(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
-      if(search) {
-          const fetchData = async () => {
-              document.title = `${search} music`
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('Not Found.')
-              }
-          }
-          fetchData()
-      }
-  }, [search])
+    if (searchTerm) {
+        setData(fetchData(searchTerm))
+    }
+}, [searchTerm])
 
   const handleSearch = (e, term) => {
       e.preventDefault()
       setSearch(term)
   }
 
-  return (
-      <div className="App">
-        <center>
-          <SearchBar handleSearch={handleSearch} />
-          {message}
-          <Gallery data={data} />
-          </center>
-      </div>
-  )
+  const renderGallery = () => {
+    if(data){
+        return (
+            <Suspense fallback={<Spinner />}>
+                <Gallery data={data} />
+            </Suspense>
+        )
+    }
+}
+
+return (
+    <div className="App">
+        <SearchBar handleSearch={handleSearch} />
+        {message}
+        {renderGallery()}
+    </div>
+)
 }
 
 export default App
---
+++ src/Components/Spinner.js
@@ -0,0 +1,11 @@
+import loading_spinner from '../1_9EBHIOzhE1XfMYoKz1JcsQ.gif'
+
+const Spinner = () => {
+    return (
+        <div>
+            <img src={loading_spinner} alt="loading..." />
+        </div>
+    )
+}
+
+export default Spinner
